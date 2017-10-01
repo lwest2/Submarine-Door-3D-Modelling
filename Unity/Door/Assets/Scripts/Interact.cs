b942a470e5c9f8c18c15d4a61d800203e7228180
lwest2 <liam.west@students.plymouth.ac.uk>
2017-10-01 23:35:11 +0100

    Sound, animations and more
    + Animations for door and handle
    + All sounds (ambient, FX)
    + Trigger door opening with 'E' key, as well as door sounds triggered
    + Camera animations
    + Materials

---
+++ Unity/Door/Assets/Scripts/Interact.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Interact : MonoBehaviour {
+
+    private float m_distance;
+
+    [SerializeField]
+    private AudioClip audioclip;
+
+    [SerializeField]
+    private AudioClip audioclip_close;
+
+    private AudioSource audio;
+    
+    [SerializeField]
+    private Transform m_target;
+
+    [SerializeField]
+    private Animator anim;
+
+    
+    // Use this for initialization
+    void Start () {
+        anim = GetComponent<Animator>();
+        audio = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        m_distance = Vector3.Distance(transform.position, m_target.position);
+
+        if (m_distance < 1.5f)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                anim.Play("Door");
+
+                if(!audio.isPlaying)
+                {
+                    StartCoroutine(SoundOut());
+                }
+            }
+        }
+    }
+
+    IEnumerator SoundOut()
+    {
+        yield return new WaitForSeconds(6);
+        audio.PlayOneShot(audioclip);
+        yield return new WaitForSeconds(14);
+        audio.PlayOneShot(audioclip_close);
+
+    }
+}
---
+++ Unity/Door/Assets/Scripts/displayUI.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class displayUI : MonoBehaviour
+{
+
+    [SerializeField]
+    private string m_myString;
+
+    [SerializeField]
+    private Text m_myText;
+
+    [SerializeField]
+    private float m_fadeTime;
+
+    [SerializeField]
+    private bool m_displayInfo;
+
+    private float m_distance;
+
+    [SerializeField]
+    private Transform m_target;
+
+    // Use this for initialization
+    void Start()
+    {
+        m_myText = GameObject.Find("Text").GetComponent<Text>();
+        m_myText.color = Color.clear;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        FadeText();
+    }
+
+    private void OnMouseOver()
+    {
+        m_displayInfo = true;
+    }
+
+    private void OnMouseExit()
+    {
+        m_displayInfo = false;
+    }
+
+    void FadeText()
+    {
+        m_distance = Vector3.Distance(transform.position, m_target.position);
+
+        if (m_distance < 1.5f)
+        {
+            if (m_displayInfo)
+            {
+                m_myText.text = m_myString;
+                m_myText.color = Color.Lerp(m_myText.color, Color.white, m_fadeTime * Time.deltaTime);
+            }
+        }
+
+        else if (!m_displayInfo)
+        {
+            m_myText.color = Color.Lerp(m_myText.color, Color.clear, m_fadeTime * Time.deltaTime);       
+        }
+      
+    }
+}
